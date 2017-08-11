72721346f9d181e2901dd0197aeddf33bf054fc1
Susana rce <ingridarce4@hotmail.com>
2017-08-11 12:49:18 -0400

    tres secciones

--- assets/js/app.js
+++ assets/js/app.js
@@ -35,4 +35,22 @@ alert(Debe ingresar un Country);
  	localStorage.setItem('contry', contry);
  }
 }
+
+$.ajax({
+	url: '/path/to/file',
+	type: 'default GET (Other values: POST)',
+	dataType: 'default: Intelligent Guess (Other values: xml, json, script, or html)',
+	data: {param1: 'value1'},
+})
+.done(function() {
+	console.log("success");
+})
+.fail(function() {
+	console.log("error");
+})
+.always(function() {
+	console.log("complete");
 });
+
+});
+
--- dist/js/script.js
+++ dist/js/script.js
@@ -20255,4 +20255,57 @@ if (jQuery) {
 
 $(document).ready(function(){
  $(".button-collapse").sideNav();
+ 
+ // validaciones
+function validarNombre(){
+var nombre = $('#nombre').val();
+if(nombre==""){
+alert(Debe ingresar un nombre);
+ }else{
+ 	localStorage.setItem('name', nombre);
+ }
+}
+
+function validarUsuario(){
+var usuario = $('#user').val();
+if(usuario==""){
+alert(Debe ingresar un usuario);
+ }else{
+ 	localStorage.setItem('user', usuario);
+ }
+}
+function validarEmail(){
+var email = $('#email').val();
+if(email==""){
+alert(Debe ingresar un email);
+ }else{
+ 	localStorage.setItem('mail', email);
+ }
+}
+function Contry(){
+var contry = $('#contry').val();
+if(contry==""){
+alert(Debe ingresar un Country);
+ }else{
+ 	localStorage.setItem('contry', contry);
+ }
+}
+
+$.ajax({
+	url: '/path/to/file',
+	type: 'default GET (Other values: POST)',
+	dataType: 'default: Intelligent Guess (Other values: xml, json, script, or html)',
+	data: {param1: 'value1'},
+})
+.done(function() {
+	console.log("success");
+})
+.fail(function() {
+	console.log("error");
+})
+.always(function() {
+	console.log("complete");
 });
+
+});
+
